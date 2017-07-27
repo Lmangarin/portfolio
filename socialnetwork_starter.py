57cd87460cd458d3250141fa80874a06b0f6fc45
Vexana <mangarinlyca@gmail.com>
2017-07-27 09:32:23 -0700

    Add files via upload

---
+++ socialnetwork_starter.py
@@ -0,0 +1,38 @@
+class User:
+    # Define the fields and methods for your object here.
+    UserName = ""
+    UserColor = 0
+    Email = ""
+    Password = ""
+
+
+    def __init__(self, name):
+		self.UserName = name
+
+    def UserColor (self, color):
+        UserColorDict = {"1": "Blue", "2": "Pink", "3" : "Red", "4" : "Orange", "5" : "Purple"}
+		if UserColor.get(typeOption):
+			self.UserColor = UserColorDict[color]
+		else:
+			self.UserColor = -1
+
+	def getName(self):
+		return self.UserColor
+
+3
+
+
+
+class Network:
+    # Define the fields and methods for your object here.
+
+
+
+def main():
+    # Define the program flow for your user interface here.
+
+
+# Runs your program.
+if __name__ == '__main__':
+    main():
+UserName = input("Please enter desired username: \n")
